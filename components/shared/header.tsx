4dd0f7137b3487fb9f1f64fec54d682527aa3db5
sergeyif1 <sergeyif1@gmail.com>
2025-02-13 03:38:02 +0200

    add the button of total cost and ShoppingCart to header

--- components/shared/header.tsx
+++ components/shared/header.tsx
@@ -4,7 +4,7 @@ import { Container } from "./index";
 import Image from "next/image";
 import Link from "next/link";
 import { Button } from "../ui";
-import { User } from "lucide-react";
+import { ShoppingCart, User } from "lucide-react";
 
 interface Props {
   className?: string;
@@ -33,6 +33,16 @@ export const Header: React.FC<Props> = ({ className }) => {
             <User size={16} />
             Войти
           </Button>
+          <div>
+            <Button className="group relative">
+              <b>520 S</b>
+              <span className="h-full w-[1px] bg-white/30 mx-3" />
+              <div className="flex items-center gap-1 transition duration-300 group-hover:opacity-0">
+                <ShoppingCart className="h-4 w-4 relative" strokeWidth={2} />
+                <b>3</b>
+              </div>
+            </Button>
+          </div>
         </div>
       </Container>
     </header>
